b4058db4dfb4823e360f4c08c9687d7d5c802dd9
Özgür Can Altınok <ozgurcanaltinok@gmail.com>
2022-08-06 18:36:06 +0300

    Add Week 4 Day 1

---
+++ week4/day1/AngularBlog/src/app/services/blog.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@microsoft/signalr';
+import { BaseService } from './base.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogService extends BaseService {
+  constructor(private base: BaseService) {
+    super(base.http);
+  }
+
+  public getPosts() {
+    return this.base.getReq('/posts');
+  }
+}
