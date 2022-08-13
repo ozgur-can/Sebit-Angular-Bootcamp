b8808edfa8a7d6d19b9552d82771ead77ad154a3
Özgür Can Altınok <ozgurcanaltinok@gmail.com>
2022-08-13 12:25:37 +0300

    Add angular blog v2 (new)

---
+++ week5/day1/AngularBlogV2/src/app/services/blog.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@microsoft/signalr';
+import { IPost } from '../interfaces';
+import { BaseService } from './base.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogService extends BaseService {
+  constructor(private base: BaseService) {
+    super(base.http);
+  }
+
+  public getPosts() {
+    return this.base.getReq('/posts');
+  }
+
+  public updatePost(id: number, data: IPost) {
+    return this.base.putReq(`/posts/${id}`, data);
+  }
+}
